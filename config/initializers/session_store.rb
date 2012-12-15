a4468c77f5422acba915436b3b54c44487c3e6a9
Computer Engineering <cn@ComClass.(none)>
2012-12-15 19:30:20 +0700

    edit timeout session

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Rottenpotatoes::Application.config.session_store 
-:cookie_store, key: '_rottenpotatoes_session'
-,:expire_after => 2.minutes
+Rottenpotatoes::Application.config.session_store :cookie_store, key: '_rottenpotatoes_session', :expire_after => 30.minutes
 
 
 # Use the database for sessions instead of the cookie-based default,
